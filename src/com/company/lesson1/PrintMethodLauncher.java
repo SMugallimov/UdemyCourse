7484142f37316f4b3718320c4ec50056ad5971fc
smugallimov <smugallimov@mail.ru>
2022-02-01 21:43:07 +0400

    Lesson one: Print and Println methods

---
+++ src/com/company/lesson1/PrintMethodLauncher.java
@@ -0,0 +1,8 @@
+package com.company.lesson1;
+
+public class PrintMethodLauncher {
+    public static void main(String[] args) {
+        PrintMethods printMethods = new PrintMethods();
+        printMethods.printer();
+    }
+}
---
+++ src/com/company/lesson1/PrintMethods.java
@@ -0,0 +1,12 @@
+package com.company.lesson1;
+
+public class PrintMethods {
+    public void printer(){
+        System.out.println("\t\t\t\tРУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("\t\t\t\t\t\t\t\tО. Хайам");
+    }
+}
